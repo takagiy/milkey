dc1cc9a168c3367c0d52ef57e340eb3c3bec32f4
takagiy <takagiy.4dev@gmail.com>
2021-02-26 16:10:40 +0900

    source: init key matrix

--- milkey/main.py
+++ milkey/main.py
@@ -1,11 +1,14 @@
 import pygame
 import sys
-import os
+from os import path
 
 KEY_SPRITE_SIZE = 256
 
-APP_WIDTH = KEY_SPRITE_SIZE * 4 + 16
-APP_HEIGHT = KEY_SPRITE_SIZE * 4 + 16
+NCOLS = 4
+NROWS = 3
+
+APP_WIDTH = KEY_SPRITE_SIZE * NCOLS + 32
+APP_HEIGHT = KEY_SPRITE_SIZE * NROWS + 32
 APP_SIZE = (APP_WIDTH, APP_HEIGHT)
 
 BG_COLOR = (255, 255, 255)
@@ -13,9 +16,29 @@ BG_COLOR = (255, 255, 255)
 pygame.init()
 app = pygame.display.set_mode(APP_SIZE)
 
+def load_image(source):
+    return pygame.image.load(path.join('images', source)).convert_alpha()
+
+class Key(pygame.sprite.Sprite):
+    def __init__(self):
+        pygame.sprite.Sprite.__init__(self)
+        self.up_img = load_image('key_black1.png')
+        self.down_img = load_image('key_black2.png')
+        self.image = self.up_img
+        self.rect = self.image.get_rect()
+
 def quit():
     pygame.quit()
     sys.exit()
+
+matrix = [[] for i in range(NCOLS)]
+keys = pygame.sprite.Group()
+
+for col in range(NCOLS):
+    for row in range(NROWS):
+        key = Key()
+        key.rect.topleft = (256 * col + 16, 256 * row + 16)
+        keys.add(key)
 
 while True:
     for event in pygame.event.get():
@@ -25,4 +48,5 @@ while True:
             if event.key == ord('q'):
                 quit()
     app.fill(BG_COLOR)
+    keys.draw(app)
     pygame.display.flip()
